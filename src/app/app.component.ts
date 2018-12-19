71299ae51f8e63770a3631c87d9e6b465a51c301
marcialwushu <marcialwushu@gmail.com>
2018-12-19 18:48:53 -0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { startOfDay } from 'date-fns';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calendar';
+  viewDate: Date = new Date();
+  events = [];
 }
+
+console.log(startOfDay);

a03a4cce5b5034fedd2223205c932c096f3cd305
marcialwushu <marcialwushu@gmail.com>
2019-01-28 14:36:32 -0200

    fullCalendar in Angular added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { startOfDay } from 'date-fns';
+import { Options } from 'fullcalendar';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,20 @@ import { startOfDay } from 'date-fns';
 export class AppComponent {
   viewDate: Date = new Date();
   events = [];
+
+  title = 'my-angular-app';
+  calendarOptions: Options;
+  ngOnInit() {
+    this.calendarOptions = {
+      editable: true,
+      eventLimit: false,
+      header: {
+        left: 'prev,next,today',
+        center: 'title',
+        right: 'month, agendaWeek,agendaDay,listMonth'
+      }
+    };
+  }
 }
 
 console.log(startOfDay);
